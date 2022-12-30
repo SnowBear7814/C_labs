4a3f92f81cb6979fb2adaada3e4a87c3732bdff8
Alexey <113058093+SnowBear7814@users.noreply.github.com>
2022-12-30 18:16:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+int main() {
+    time_t t = time(NULL);
+    struct tm* current_time = localtime(&t);
+    FILE*output = fopen("Best_time.txt", "w");
+    for (int i = 0; i<10; i++) {
+        fprintf(output, "%02d-%02d-%04d \n",
+                current_time->tm_mday + i,
+                current_time->tm_mon + 1,
+                current_time->tm_year + 1900);
+    }
+    fclose(output);
+    return 0;
+}
