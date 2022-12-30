2773b87ccea12ecaf124a589f19cec914fe93294
Alexey <113058093+SnowBear7814@users.noreply.github.com>
2022-12-30 17:48:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Rhombus{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+} rhom;
+void area(struct Rhombus){
+    float ar = abs(rhom.x1 - rhom.x2) * abs(rhom.y1 - rhom.y2);
+    printf("%f\n", ar);
+}
+void perimeter(struct Rhombus){
+    float per = 2 * abs(rhom.x1 - rhom.x2) + 2 * abs(rhom.y1 - rhom.y2);
+    printf("%f", per);
+
+};
+int main(){
+    scanf("%d", &rhom.x1);
+    scanf("%d", &rhom.x2);
+    scanf("%d", &rhom.y1);
+    scanf("%d", &rhom.y2);
+    area(rhom);
+    perimeter(rhom);
+    return 0;
+}
