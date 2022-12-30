7ccf0491f5c793240514d42bcb6d05a067ac6d3c
Alexey <113058093+SnowBear7814@users.noreply.github.com>
2022-12-30 15:31:41 +0300

    Add files via upload

---
+++ Lab_3_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//мой вариант - 15
+int main() {
+    int a, b, c;
+    scanf("%x", &a);
+    printf("10-num: %d\n", a);
+    printf("8-num: %o\n", a);
+    printf("16-num: %x\n", a);
+    printf("8-num: %o >> 3 == %o\n", a, a >> 3);
+    printf("16-num: %x >> 3 == %x\n", a, a >> 3);
+    printf("8-num: ~ %o = %o\n", a, ~a);
+    printf("16-num: ~ %x = %x\n", a, ~a);
+    b = ('%o\n', a);
+    c = ('%x\n', a);
+    printf("8-num||16-num: %o||%x == %d\n", a, a,b|c);
+    return 0;
+}
